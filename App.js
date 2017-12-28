4743faabe03436dfc3b215554d9b1a36e5bfa100
Mauricio Mercado <mau@maumercado.com>
2017-12-27 22:17:13 -0800

    Initial Commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { AppRegistry, View } from "react-native";
+
+const App = () => {
+    return <View />;
+};
+
+export default App;
+
+AppRegistry.registerComponent("tech_stack", () => App);
